5f93ab402412742f71844bd3db8d95f6acf1668e
Thiago Medeiros <thiago@tuta.io>
2023-01-22 22:28:14 -0300

    top ok. header and banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ module.exports = {
     "./assests/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        banner: "url('/assets/img/bg-banner.jpg')",
+        search: "url('/assets/img/icon-search-gray.svg')"
+      },
+      colors: {
+        azul :{
+          normal: '#2A7AE4'
+        }
+      },
+      fontFamily: {
+        primary: ['Raleway']
+      },
+      screens: {
+        'xl2': '1440px',
+        // => @media (min-width: 640px) { ... }
+      }
+    },
   },
   plugins: [],
 }
